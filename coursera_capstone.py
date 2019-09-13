214334ae367c6632bd640ac7a5400bb03fe8eb53
Jay Okawachi <jay.okawachi@hginsights.com>
2019-09-13 07:22:09 -0700

    first commit on capstone

---
+++ coursera_capstone.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Coursera Capstone Project
+# 
+# #### This notebook is primarly used for a capstone project
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
